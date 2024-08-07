ba25ec526582ad9c96c97bbf50c414d62eb05a24
p4r53c <n1chr0mix@gmail.com>
2024-08-07 17:40:13 +0300

    HW 14 Fix BS

--- src/test/java/io/p4r53c/telran/BrokenFloorTest.java
+++ src/test/java/io/p4r53c/telran/BrokenFloorTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import io.p4r53c.telran.exps_iterator.BallBrokenFloor;
-import io.p4r53c.telran.exps_iterator.exceptions.BrokenBallException;
 
 class BrokenFloorTest {
 
@@ -14,15 +13,15 @@ class BrokenFloorTest {
     // HW 14
     private int getMinBrokenFloor(BallBrokenFloor ballBrokenFloor) {
         int low = 1;
-        int high = ballBrokenFloor.getMinBrokenFloor();
+        int high = Integer.MAX_VALUE;
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
             try {
                 ballBrokenFloor.checkFloor(mid);
                 low = mid + 1;
-            } catch (BrokenBallException ex) {
-                logger.debug(ex.getMessage());
+            } catch (Exception ex) {
+                logger.debug(ex.getMessage()); // Message: Null is obviously, but it helps to see too many iterations
                 high = mid - 1;
             }
         }
